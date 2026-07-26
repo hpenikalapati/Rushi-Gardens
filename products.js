9cc38dbc9bb2b5d993674b5bd33c910c719dc7b4
Hansita Penikalapati <138614461+hpenikalapati@users.noreply.github.com>
2026-07-26 19:46:34 -0400

    Update products.js

--- products.js
+++ products.js
@@ -1,11 +1,23 @@
 const defaultProducts = [
   { id: 'green-okra', name: 'Green Okra', pricePerLb: 3.50, inStock: true },
-  { id: 'eggplant', name: 'Eggplant', pricePerLb: 1.99, inStock: true },
-  { id: 'gongura', name: 'Gongura', pricePerLb: 7, inStock: true },
-  { id: 'mango', name: 'Mangos', pricePerLb: 6, inStock: true },
-  { id: 'tomato', name: 'Tomatoes', pricePerLb: 1.49, inStock: true },
-  { id: 'guava', name: 'Guavas', pricePerLb: 3.99, inStock: true },
-  { id: 'lychee', name: 'Lychees', pricePerLb: 5.99, inStock: false }
+  { id: 'red-okra', name: 'Red Okra', pricePerLb: 3.00, inStock: true },
+  { id: 'eggplant', name: 'Eggplant', pricePerLb: 3.00, inStock: true },
+  { id: 'gongura', name: 'Gongura', pricePerLb: 7.00, inStock: true },
+  { id: 'green-chilies', name: 'Green Chilies', pricePerLb: 5.00, inStock: true },
+  { id: 'tomato', name: 'Indian Tomatoes', pricePerLb: 3.00, inStock: true },
+  { id: 'cluster-beans', name: 'Cluster Beans', pricePerLb: 4.00, inStock: true },
+  { id: 'ridge-gourd', name: 'Ridge Gourd', pricePerLb: 2.00, inStock: true },
+  { id: 'mango', name: 'Mangos', pricePerLb: 6.00, inStock: true },
+  { id: 'suppota', name: 'Suppota', pricePerLb: 6.00, inStock: true },
+  { id: 'sugar-apple', name: 'Sugar Apple', pricePerLb: 15, inStock: true },
+  { id: 'guava', name: 'Guavas', pricePerLb: 5.00, inStock: true },
+  { id: 'lychee', name: 'Lychees', pricePerLb: 11.00, inStock: false },
+  { id: 'banana', name: 'Indian Karpuravali Banana', pricePerLb: 1.49, inStock: false },
+  { id: 'jackfruit-whole', name: 'Whole Jackfruit', pricePerLb: 1.00, inStock: true },
+  { id: 'jackfruit-pieces', name: 'Jackfruit Pieces', pricePerLb: 1.25, inStock: true },
+  { id: 'tindora', name: 'Tindora', pricePerLb: 4.00, inStock: true },
+  { id: 'paan-leaves', name: 'Paan Leaves', pricePerLb: 0.20, inStock: true },
+  { id: 'eggs', name: 'Eggs', pricePerLb: 9.00, inStock: true },
 ];
 
 // Helper functions to manage LocalStorage persistence
